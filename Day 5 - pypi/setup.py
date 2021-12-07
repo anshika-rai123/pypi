3f6dac76312055e4c346fc5ce711f1bdc865fac6
anshika-rai123 <53621887+anshika-rai123@users.noreply.github.com>
2021-12-07 17:37:06 +0530

    Add files via upload

---
+++ Day 5 - pypi/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'anshika', # while installing pacakge, e.g., matplotlib...
+    version = '0.0.1',
+    description = 'Simple Bank Account.',
+    long_description = open('Readme.txt').read(),
+    url = 'https://github.com/anshika-rai123/',
+    author = 'Anshika Rai',
+    keywords = ['account', 'bank', 'class', 'ideationology'],
+    packages = ['anshi'], # while importing package, e.g., pyplot...
+)
